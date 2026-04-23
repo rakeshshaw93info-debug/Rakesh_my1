5fe07d6013b68aacb02b294a562d319d8784f4d2
rakeshshaw93info-debug <rakesh.shaw93.info@gmail.com>
2026-04-23 17:27:08 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler
 
 TOKEN = os.environ.get("8671139635:AAEfCLWXxRj3GRdBhEVx4ThXqOtOnKrRGQg")
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello 👋 Your bot is LIVE!")
+async def start(update: Update, context):
+    await update.message.reply_text("Hello Sir, I'm Rakesh. How may I help you?")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("start", start))
